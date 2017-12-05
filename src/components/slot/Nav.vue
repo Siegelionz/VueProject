315e29f22de7fbeff0eb32c79b5c9f3fdf9aad81
zhangyutian <1097776757@qq.com>
2017-12-05 22:53:04 +0800

    更新main文件，添加全局组件nav

---
+++ src/components/slot/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="cms-s-header">
+            <h1 @click="goback">&nbsp<</h1>
+            <h2 class="page-title">{{title}}</h2>
+        </div> 
+    </div>
+</template>
+<script>
+    export default {
+        props:['title'],
+        name:'new-nav',
+        data(){
+            return {
+                val:''
+            }
+        },
+        methods:{
+            goback:function(){
+                this.$router.go(-1);
+            }
+        }
+    }
+</script>
+<style scoped>
+    .cms-s-header{
+        background-color: #f7f7f7;
+    }
+    .tmpl h2,h1{
+        display: inline-block;
+    }
+    h2{
+        margin-left:99px;
+    }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //vue  start
 import Vue from 'vue';
+import Moment from 'moment';
 
 // 引入组件 start
 import App from './components/App.vue';
@@ -7,13 +8,27 @@ import Home from './components/Home/Home.vue';
 import Vip from './components/Vip/Vip.vue';
 import Shopcart from './components/Shopcart/Shopcart.vue';
 import Find from './components/Find/Find.vue';
+import NewsList from './components/News/NewsList.vue';
+import NewsDetail from './components/News/NewsDetail.vue';
+import PhotoList from './components/Photo/PhotoList.vue';
+import PhotoDetail from './components/Photo/PhotoDetail.vue';
 //vue-router  start
 
 //引入组件并全局注册
 import MyUl from './components/slot/MyUl.vue';
 import MyLi from './components/slot/MyLi.vue';
+import MyNav from './components/slot/Nav.vue';
 Vue.component(MyUl.name,MyUl);
 Vue.component(MyLi.name,MyLi);
+Vue.component(MyNav.name,MyNav)
+
+//注册过滤器
+Vue.filter('filterTime',function(value){
+    return Moment(value).format('YYYY-MM-DD');
+})
+Vue.filter('filterText',function(value){
+    return value.substr(0,14)+'..';
+})
 
 //引入vue-router
 import VueRouter from 'vue-router';
@@ -23,10 +38,15 @@ Vue.use(VueRouter);
 let router = new VueRouter();
 //配置路由规则
 router.addRoutes([
-    { name:'home',path:'/home',component:Home },
-    { name:'vip',path:'/vip',component:Vip },
-    { name:'shopcart',path:'/shopcart',component:Shopcart },
-    { name:'find',path:'/find',component:Find }
+    { path:'/',component:Home },   //重定向
+    { name:'home',path:'/home',component:Home },//主页组件
+    { name:'vip',path:'/vip',component:Vip },//会员组件
+    { name:'shopcart',path:'/shopcart',component:Shopcart },//购物车组件
+    { name:'find',path:'/find',component:Find },//查找组件
+    { name:'newslist',path:'/newslist',component:NewsList },//新闻列表组件
+    { name:'newsdetail',path:'/newsdetail',component:NewsDetail },//新闻详情组件
+    { name:'photolist',path:'/photolist/:listid',component:PhotoList },//图文列表组件
+    { name:'photodetail',path:'/photodetail/:detailid',component:PhotoDetail }//图文详情列表
 ])
 
 //vue-router end
@@ -55,6 +75,10 @@ Vue.prototype.$axios = Axios;
 Axios.defaults.baseURL = 'http://vue.studyit.io/api/'
 
 //axios end
+
+//引入vue-preview
+import VuePreview from 'vue-preview'
+Vue.use(VuePreview)
 
 new Vue({
     el:'#app',
